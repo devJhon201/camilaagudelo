620eeb759e6799babe9a46f1e7788c95da18df40
Jhon C <82723270+devJhon201@users.noreply.github.com>
2021-05-26 18:30:23 +0200

    Renponsividad agregada para conputadores y tabletas

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
-const headerStyle = document.getElementById("header");
-headerStyle.style.height = window.innerHeight + "px";
+// Header usa el espacio completo de la ventana
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+	/* The viewport is less than, or equal to, 700 pixels wide */
+	const headerStyle = document.getElementById("header");
+	headerStyle.style.height = window.innerHeight + "px";
+} else {
+	/* The viewport is greater than 700 pixels wide */
+}
