c4ab1503fe317885b689f6d9898985dbbc81f8f6
Jhon C <82723270+devJhon201@users.noreply.github.com>
2021-05-27 16:51:17 +0200

    domcontentloaded agregado en javascrip

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
-// Header usa el espacio completo de la ventana
+window.addEventListener("DOMContentLoaded", (event) => {
+	// Header usa el espacio completo de la ventana
 
-if (window.matchMedia("(max-width: 768px)").matches) {
-	/* The viewport is less than, or equal to, 700 pixels wide */
-	const headerStyle = document.getElementById("header");
-	headerStyle.style.height = window.innerHeight + "px";
-} else {
-	/* The viewport is greater than 700 pixels wide */
-}
+	if (window.matchMedia("(max-width: 768px)").matches) {
+		/* The viewport is less than, or equal to, 700 pixels wide */
+		const headerStyle = document.getElementById("header");
+		headerStyle.style.height = window.innerHeight + "px";
+	} else {
+		/* The viewport is greater than 700 pixels wide */
+	}
+});
